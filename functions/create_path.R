4a7cc3b2011a12c7d111ae2a55f8aa50a86e6c10
sergrabo <sergrabo4@gmail.com>
2022-06-09 16:38:30 +0200

    Function to help create the path where results will be stored

---
+++ functions/create_path.R
@@ -0,0 +1,31 @@
+#     create_path
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#' @title 
+#' @description 
+#' @param 
+#' @return 
+#' @author Sergio Gracia
+#' @references 
+#' 
+#' 
+
+create_path <- function(th){
+  specify_decimal <- function(x, k) trimws(format(round(x, k), nsmall=k))
+  th.form <- specify_decimal(th, 2)
+  decimal <- substr(th.form, 3, 4)
+  path <- paste0("/th_0",decimal)
+  return(path)
+}
