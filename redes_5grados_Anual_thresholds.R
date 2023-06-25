221313317772a68ccee777f59c3fc81164bf177b
sergrabo <sergrabo4@gmail.com>
2023-06-25 19:12:18 +0200

    Final tweaks to measures and plots

--- redes_5grados_Anual_thresholds.R
+++ redes_5grados_Anual_thresholds.R
@@ -17,6 +17,7 @@ library(RColorBrewer)
 library(sp)
 library(grid)
 library(gridExtra)
+library(xtable)
 
 # Directorio de trabajo
 setwd("C:/Users/sergr/Dropbox/TFM_Sergio_Gracia")
@@ -29,6 +30,7 @@ sapply(list.files("./scripts/MastersThesis/functions/", full.names = TRUE), "sou
 # load("./Rdata/ba5degAnom.Rdata", verbose = TRUE)
 load("./Rdata/ba5deg_anom_standarize.Rdata", verbose = TRUE)
 ba.5deg.std.anom <- ba.5deg.std.anom.standarize
+rm(ba.5deg.std.anom.standarize)
 load("./Rdata/mask.Rdata", verbose = TRUE)
 
 ################# Estudio por thresholds ########################
@@ -68,38 +70,27 @@ atts$thresholds <- ths
 
 rm(all_data)
 
-## Communities ###
+############## Communities ##################
+
+# Plot and save dendrograms
 for (i in ths[9:20]) {
   th <- i
   c <- comms[[which.min(abs(ths-th))]]
   d <- as.hclust(c)
-  pdf(paste0("graphs/","dendrogram_CN_",th,".pdf"), width = 8.27, height = 11.69)
-  plot(d, cex = .4, main = paste("CN",th,"threshold"), sub = "", xlab = "", lwd = 0.1)
-  abline(h=644-nrow(c$merges), col = "red", lwd = 1.5)
-  abline(h=nrow(c$merges), col = "blue")
+  pdf(paste0("graphs/5deg_Anual_dist0/dendrograms/","dendrogram_CN_",th,".pdf"), width = 8.27, height = 11.69)
+  plot(d, labels = FALSE, cex = .4, main = bquote("Dendrogram for " ~ tau[c] ~ "=" ~ .(th)), sub = "", xlab = "", lwd = 0.1)
+  # abline(h=645-nrow(c$merges), col = "red", lwd = 1.5)
+  # abline(h=nrow(c$merges)-45, col = "blue")
   dev.off()
 }
 
-
-cuts <- 500#
-
 th <- 0.7
 c <- comms[[which.min(abs(ths-th))]]
-n <- wnet[[which.min(abs(ths-th))]]
-k <- degree(n$graph)
-k.null <- which(k==0)
-plot_communities(c, ref.grid = ba.5deg.std.anom, ref.mask = mask, th = 2, cuts = 3)
+# n <- wnet[[which.min(abs(ths-th))]]
+# k <- degree(n$graph)
+# k.null <- which(k==0)
+plot_communities(c, ref.grid = ba.5deg.std.anom, ref.mask = mask, th = 10, cuts = 3, cor.th = th)
 
-## TH = 0.7
-# cuts --> ncomm >=2 pixel
-# 300 --> 50
-# 302 --> 50
-## ~~~~~~~WARNING
-# 303 --> 50
-# 305 --> 52
-# 310 --> 57
-# 325 --> 72
-# 350  --> 88
 
 # Comparison with random net
 set.seed(4)
@@ -127,14 +118,42 @@ rnet.plot <- lapply(rnet, FUN = as.graphObj, ref.graphObj = unwnet[[15]])
 
 ################# SPATIAL PLOT #################
 # th = 0.5, 0.7, 0.8
-th <- 0.5
+th <- 0.8
 i <- which.min(abs(ths-th))
+# graphObj <- wnet[[i]]
 attr(rnet.plot[[i]], "threshold") <- th
 
 graph_world_network(rnet.plot[[i]])
 graph_world_network(unwnet[[i]])
 graph_world_network(wnet[[i]])
 
+# Measures
+plot.path <- paste0("./graphs/5deg_Anual_dist0/measures_th0", 10*th, ".pdf")
+pdf(file = plot.path, width = 7, height = 7)
+plot_measures(wnet[[i]], mute = TRUE)
+dev.off()
+
+# Tabla de atributos
+# a <- attributes(unwnet[[i]])[-(1:6)]
+a <- atts
+max.id <- which(1:length(a$total_range_dist)%%2 == 0)
+min.id <- which(1:length(a$total_range_dist)%%2 == 1)
+
+a$total_max_dist <- a$total_range_dist[max.id]
+a$pos_max_dist <- a$pos_range_dist[max.id]
+a$neg_max_dist <- a$neg_range_dist[max.id]
+
+a$total_min_dist <- a$total_range_dist[min.id]
+a$pos_min_dist <- a$pos_range_dist[min.id]
+a$neg_min_dist <- a$neg_range_dist[min.id]
+
+a$total_range_dist <- NULL
+a$pos_range_dist <- NULL
+a$neg_range_dist <- NULL
+a $ thresholds<- NULL
+
+df <- a %>% data.frame()
+print(xtable(df, type = "latex"), file = "tablaAtribs.tex")
 ################################################
 
 ################# CLUSTERING COEFFICIENT AND DIAMETER VS THREHSOLD #################
@@ -156,9 +175,9 @@ par(new=TRUE)
 # Plot the second plot and put axis scale on right
 plot(ths, atts$diam, axes = FALSE, col = "darkorange1", xlab = "", ylab = "", ylim = c(-0.05,max(c(max(rand.diam), max(atts$diam)))),
      pch = 16, type = "b", lwd = 2)
-points(ths[complex.conn==Inf], atts$diam[complex.conn==Inf], col = "darkorange1", pch = 8, cex = 2, lwd = 2)
+points(ths[complex.conn==Inf | complex.conn == 645], atts$diam[complex.conn==Inf | complex.conn == 645], col = "darkorange1", pch = 8, cex = 2, lwd = 2)
 points(ths, rand.diam, col = "darkorange3", pch = 16, type = "b", lwd = 2)
-points(ths[rand.conn==Inf], rand.diam[rand.conn==Inf], col = "darkorange3", pch = 8, cex = 2, lwd = 2)
+points(ths[rand.conn==Inf| rand.conn == 645], rand.diam[rand.conn==Inf | rand.conn == 645], col = "darkorange3", pch = 8, cex = 2, lwd = 2)
 
 ## a little farther out (line=4) to make room for labels
 axis(4, at = 0:max(c(max(rand.diam), max(atts$diam))), ylim = c(0,max(c(max(rand.diam), max(atts$diam)))), col = "red", col.axis = "red", las = 1)
@@ -166,7 +185,7 @@ mtext("Diameter", side = 4, col = "red", line = 4)
 
 ## Draw the time axis
 axis(1, ths)
-mtext("Thresholds",side=1,col="black",line=2.5)  
+mtext("Correlation threshold",side=1,col="black",line=2.5)  
 
 ## Add Legend
 legend("topright",
@@ -195,14 +214,14 @@ points(atts$thresholds, atts$neg_mean_dist, col = "red", pch = 16, type = "b")
 text(ths, -0.05, atts$total_edges, col= "blue", cex = 0.8, lwd = 2)
 box()
 axis(1, ths)
-mtext("Thresholds",side=1,col="black",line=2.5) 
+mtext("Correlation threshold",side=1,col="black",line=2.5) 
 axis(2, ylim = c(0,10000), col="black", las = 1)
-mtext("Mean edge distance", side = 2, line = 4)
+mtext("Mean link distance", side = 2, line = 4)
 ## Add Legend
 legend(x = 0.15, y = 4000,
-       legend = c("All edges",
-                  "Positively correlated edges",
-                  "Negatively correlated edges"),
+       legend = c("All links",
+                  "Positively correlated links",
+                  "Negatively correlated links"),
        text.col = c("black",
                     "blue",
                     "red"),
